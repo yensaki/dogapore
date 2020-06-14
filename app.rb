b385de30fefdeab348ef45f0b689bd48d39bd40b
yensaki <mov.an.double@gmail.com>
2020-06-14 14:06:06 +0900

    Control case of no request.body

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 
 get '/' do
-  request.body.rewind
+  # request.body.rewind
+  if request.body.to_s == ""
+    return "no body"
+  end
   data = JSON.parse request.body.read
   "Hello #{data}!\n"
 end
